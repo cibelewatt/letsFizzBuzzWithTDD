bfe2636e163a2f5d9e92515183a490d8cca6f62c
Cibele Watt <cibelewatt@gmail.com>
2021-10-19 21:53:53 -0300

    create letsfizzbuzz project

---
+++ LetsFizzBuzzTDD/LetsFizzBuzz.UnitTests/FizzBuzzTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LetsFizzBuzz.UnitTests
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
