e40aa0b8ce0bb0d5af1ca4593bb4cb57eb5a1087
João Estevan Barbosa <jestevan12@gmail.com>
2021-10-20 19:58:36 -0300

    Starting FIzzBuzz with Tests and Console

---
+++ LetsFizzBuzzTDD/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LetsFizzBuzz.Domain;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz");
+            Console.WriteLine("Se ele for divisível por 3, aparecerá Fizz");
+            Console.WriteLine("Se ele for divisível por 5, aparecerá Buzz");
+            Console.WriteLine("Se ele for divisível por 3 e 5, aparecerá FizzBuzz");
+            Console.WriteLine("Não sendo divisível por 3 e 5, retornará o número digitado");
+            Console.WriteLine("Enter a number");
+
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Returning: {FizzBuzzDomain.GetValue(input)}");
+        }
+    }
+}
---
+++ LetsFizzBuzzTDD/LetsFizzBuzz.Domain/FizzBuzzDomain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsFizzBuzz.Domain
+{
+    public class FizzBuzzDomain
+    {
+        public static string GetValue(int input)
+        {
+            if (input % 3 != 0 && input % 5 != 0)
+            {
+                return input.ToString();
+            }
+            if (input % 3 == 0 && input % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else return input.ToString();
+        }
+    }
+}
