f9422b7b0067ae21be6cbb7188b71f272e6ce3ee
João Estevan Barbosa <jestevan12@gmail.com>
2021-10-20 22:31:46 -0300

    Dependency injection

--- LetsFizzBuzzTDD/ConsoleApp/Program.cs
+++ LetsFizzBuzzTDD/ConsoleApp/Program.cs
@@ -1,22 +1,43 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions.Common;
 using LetsFizzBuzz.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Client;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly CancellationTokenSource tokenSource = new();
+        static Task Main(string[] args)
         {
-            Console.WriteLine("Welcome to FizzBuzz");
-            Console.WriteLine("Se ele for divisível por 3, aparecerá Fizz");
-            Console.WriteLine("Se ele for divisível por 5, aparecerá Buzz");
-            Console.WriteLine("Se ele for divisível por 3 e 5, aparecerá FizzBuzz");
-            Console.WriteLine("Não sendo divisível por 3 e 5, retornará o número digitado");
-            Console.WriteLine("Enter a number");
+        
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.ConfigureServices((_, services) =>
+            {
+                services.AddTransient<FizzBuzzDomain>();
+                services.AddHostedService<Startup>();
+                services.AddSingleton(tokenSource);
+            });
 
-            var input = Convert.ToInt32(Console.ReadLine());
+            using var host = hostBuilder.Build();
 
-            Console.Write($"Returning: {FizzBuzzDomain.GetValue(input)}");
+            return host.RunAsync(tokenSource.Token);
         }
+
+        //Console.WriteLine("Welcome to FizzBuzz");
+        //Console.WriteLine("Number divisible by 3, will appear Fizz");
+        //Console.WriteLine("Number divisible by 5, will appear Buzz");
+        //Console.WriteLine("Number both divisible by 3 and 5, will appear FizzBuzz");
+        //Console.WriteLine("Number cannot be divisible by 3 and 5, will appear the number");
+        //Console.WriteLine("Enter a number");
+
+        //var input = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write($"Returning: {FizzBuzzDomain.GetValue(input)}");
     }
+    
 }
---
+++ LetsFizzBuzzTDD/ConsoleApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LetsFizzBuzz.Domain;
+using Microsoft.Extensions.Hosting;
+
+
+namespace ConsoleApp
+{
+    public class Startup : IHostedService
+    {
+        private readonly FizzBuzzDomain _fizz;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public Startup(FizzBuzzDomain fizz, CancellationTokenSource cancellationTokenSource)
+        {
+            _fizz = fizz;
+            _cancellationTokenSource = cancellationTokenSource;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+
+
+            Console.WriteLine("All Fizzbuzz results from Zero to One Hundred"); 
+
+            for (int i = 0; i <= 100; i++)
+            {
+                var output = _fizz.GetValue(i);
+                Console.WriteLine(output);
+            }
+            
+
+            _cancellationTokenSource.Cancel();
+            return ;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Parando a aplicação...");
+            return;
+        }
+    }
+}
