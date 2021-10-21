60f417899317ef05ef0a6e27d27dbf548e912c5f
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-10-21 15:27:13 -0300

    Clean Code Program

--- LetsFizzBuzzTDD/ConsoleApp/Program.cs
+++ LetsFizzBuzzTDD/ConsoleApp/Program.cs
@@ -28,16 +28,7 @@ namespace ConsoleApp
             return host.RunAsync(tokenSource.Token);
         }
 
-        //Console.WriteLine("Welcome to FizzBuzz");
-        //Console.WriteLine("Number divisible by 3, will appear Fizz");
-        //Console.WriteLine("Number divisible by 5, will appear Buzz");
-        //Console.WriteLine("Number both divisible by 3 and 5, will appear FizzBuzz");
-        //Console.WriteLine("Number cannot be divisible by 3 and 5, will appear the number");
-        //Console.WriteLine("Enter a number");
-
-        //var input = Convert.ToInt32(Console.ReadLine());
-
-        //Console.Write($"Returning: {FizzBuzzDomain.GetValue(input)}");
+       
     }
     
 }
